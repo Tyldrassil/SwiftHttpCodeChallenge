13edfde245fb77aac921906d8156f138b45a7057
Tyldrassil <hyperkirbytwo@gmail.com>
2024-01-27 11:38:33 +0100

    Finished the code task with a fresh brain

--- SwiftHttpChallenge/SwiftHttpChallenge/InputView.swift
+++ SwiftHttpChallenge/SwiftHttpChallenge/InputView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Thomas Ditman on 26/01/2024.
 //
-// I likely would not use Swift for this kind of task, it seems more like something that should be accomplished by a backend. This may just be due to personal inexperience with using swift for HTTP requests though. I think this might be a bit more comfortable to write in Golang.
+//
 
 import SwiftUI
 
@@ -32,7 +32,7 @@ struct InputView: View {
             })
             
             if (resultCount != 0) {
-                Text("The number of occurrences of the input in the wikipedia response is" + String(resultCount))
+                Text("The number of occurrences of the input in the wikipedia response is: " + String(resultCount))
             }
         }
         
@@ -41,47 +41,39 @@ struct InputView: View {
     //Wasn't able to test the query from online, but tested the resulting code.
     func sendQuery() {
         
-        //Make sure input is valid, replace space with %20
-        //let validInput = inputField.replacingOccurrences(of: " ", with: "%20")
-        
         //Build query path
         let urlPath = "https://en.wikipedia.org/w/api.php?action=parse&section=0&prop=text&format=json&page=" + inputField
         
         let url = URL(string: urlPath)!
         
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+        
+        //This builds a request for the raw data from the page. At this point we could either write code to get the data parsed as JSON, or use this solution.
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else { return }
             
-            if let error = error {
-                //client
-                return
+            //Converts response into a string
+            htmlResponse = String(data: data, encoding: .utf8)!
+            
+            var searchInput: String = ""
+            
+            //This cuts off only the text field (And what comes after it, not perfect solution that works for any API). And counts occurrences of the input in it. Case sensitive.
+            if let parsedForTextField = htmlResponse.range(of: "\"text\":") {
+                searchInput = String(htmlResponse[parsedForTextField.upperBound...])
             }
             
-            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
-                //server error
-                return
-            }
-            //TODO: TEST
-            if let mimeType = httpResponse.mimeType, mimeType == "text/html",
-               let data = data,
-               let string = String(data: data, encoding: .utf8) {
-                DispatchQueue.main.async {
-                    htmlResponse = string
-                }
-            }
+            //Updates result
+            resultCount = subStringCounter(fullString: searchInput, subString: inputField)
             
+            /**
+             Alternatively for just the Wikipedia API, instead of cutting off after the text field, you could also just baseline remove one more instance of the input, since it only happens once before the text field due to the title header.
+             
+             But this should really be done by parsing the JSON instead of using the raw data that comes in.
+             */
         }
         
         //Send GET request
         task.resume()
-        
-        //Parse results
-        resultCount = subStringCounter(fullString: htmlResponse, subString: inputField)
-        
-        
     }
-    
-        
-    
 }
 
 #Preview {
